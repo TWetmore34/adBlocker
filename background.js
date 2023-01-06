f2a3b5d9617f48630ddd37af26e6c703f9983b8b
TWetmore34 <tjwetmore34@gmail.com>
2023-01-06 18:38:41 -0500

    added small list of blocked advertisers

--- background.js
+++ background.js
@@ -1,8 +1,22 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
+        console.log(details)
         return { cancel: true }
     },
     // this arr uses regex
-    {urls: ["*://*.zedo.com/*"]},
+    {urls: defaultFilters},
     ["blocking"]
 )
