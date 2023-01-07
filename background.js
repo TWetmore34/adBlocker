e518f3abff89a7e19aba649b98be2443903ef282
TWetmore34 <tjwetmore34@gmail.com>
2023-01-06 19:40:23 -0500

    added dynamic additions to defaultFilters array

--- background.js
+++ background.js
@@ -8,12 +8,27 @@ const defaultFilters = [
 	"*://*.exponential.com/*",
 	"*://*.quantserve.com/*",
 	"*://*.scorecardresearch.com/*",
-	"*://*.zedo.com/*",
+	"*://*.zedo.com/*"
 ]
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        console.log(details)
+	function(details) {
+		let url = details.url
+		console.log(details.url, defaultFilters)
+		let start = url.indexOf("//") + 2
+		let end;
+		for(let i = start; i < url.length; i++) {
+			if(url[i] === "/") {
+				end = i
+        		break
+    		}
+		}
+		urlStart = "*://*." + url.slice(start, end+1) + "*"
+		if(!defaultFilters.includes(urlStart)){
+			console.log(urlStart)
+			defaultFilters.push(urlStart)
+		}
+		console.log(defaultFilters)
         return { cancel: true }
     },
     // this arr uses regex
